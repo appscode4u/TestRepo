401611cb00b60fdd74a6d2e7af20e7544528c48c
Uberr0 <kamil.michalek@interia.eu>
2020-12-03 22:21:21 +0100

    test

--- TestRepo/Program.cs
+++ TestRepo/Program.cs
@@ -6,7 +6,7 @@ namespace TestRepo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! To jest mój branch");
         }
     }
 }
